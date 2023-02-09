8103c4c3b14adb6af6511377e8a8727ff8f9b68b
YuriMykhalchenko <108886900+YuriMykhalchenko@users.noreply.github.com>
2023-02-09 17:07:09 +0100

    update

---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Item, DeleteButton } from './ContactsList.styled';
+import { nanoid } from 'nanoid';
+
+export const ContactsList = ({ contacts, removeContact }) => {
+  return contacts.map(({ id, name, number }) => (
+    <Item key={id ? id : nanoid(4)}>
+      {name}: {number}
+      <DeleteButton type="button" onClick={() => removeContact(id)}>
+        Delete
+      </DeleteButton>
+    </Item>
+  ));
+};
+
+ContactsList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  removeContact: PropTypes.func,
+};
---
+++ src/components/ContactsList/ContactsList.styled.js
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+import { theme } from '../../utils/theme';
+
+export const Item = styled.li`
+  padding-top: ${theme.space.secondary * 2}px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: ${theme.space.secondary * 2}px;
+  font-size: ${theme.typography.title};
+  color: ${theme.colors.text};
+`;
+
+export const DeleteButton = styled.button`
+  font-size: ${theme.typography.text};
+  text-align: center;
+  padding: ${theme.space.secondary}px;
+  cursor: pointer;
+  box-shadow: 1px 3px 5px ${theme.colors.buttonDelete};
+  border: none;
+  border-radius: 5px;
+  background-color: ${theme.colors.buttonDelete};
+  &:hover,
+  &:focus {
+    background-color: ${theme.colors.buttonDeleteHover};
+  }
+`;
