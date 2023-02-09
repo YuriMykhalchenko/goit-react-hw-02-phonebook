5fc3a1f8dd92243bc3f8f4339c7a7d1ab2b230e3
YuriMykhalchenko <108886900+YuriMykhalchenko@users.noreply.github.com>
2023-02-09 18:51:05 +0100

    update

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -6,7 +6,7 @@ import { nanoid } from 'nanoid';
 export const ContactsList = ({ contacts, removeContact }) => {
   return contacts.map(({ id, name, number }) => (
     <Item key={id ? id : nanoid(4)}>
-      {name}: {number}
+      ~ {name}: {number}
       <DeleteButton type="button" onClick={() => removeContact(id)}>
         Delete
       </DeleteButton>
